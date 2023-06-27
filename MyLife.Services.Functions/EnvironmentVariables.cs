0018367ad0c0ee67a0772f954f5d08e944fe617f
Dade Cook <dadecook@gmail.com>
2023-06-26 21:06:54 -0400

    first commit

---
+++ MyLife.Services.Functions/EnvironmentVariables.cs
@@ -0,0 +1,7 @@
+﻿namespace MyLife.Services.Functions;
+
+internal static class EnvironmentVariables
+{
+    internal const string NotionAccessToken = "NOTION_ACCESS_TOKEN";
+    internal const string NotionBillConfigurationDatabaseId = "NOTION_BILL_CONFIGURATION_DATABASE_ID";
+}
---
+++ MyLife.Services.Functions/FunctionHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLife.Services.Functions;
+
+internal static class FunctionHelpers
+{
+    internal static string GetEnvironmentVariable(string name)
+    {
+        return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+    }
+}
