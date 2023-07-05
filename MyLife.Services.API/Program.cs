d7e00071750430ed49031a4baa9e41f86ea65ed8
Dade Cook <dadecook@gmail.com>
2023-07-05 10:10:13 -0400

    Downgraded to .Net 7

--- MyLife.Services.API/Program.cs
+++ MyLife.Services.API/Program.cs
@@ -15,6 +15,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -40,6 +45,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseCors("AllowAll");
 }
 
 app.UseHttpsRedirection();
