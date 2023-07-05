624c84f3654627ec5e07ab763f138f5fb08f7a53
Dade Cook <dadecook@gmail.com>
2023-07-05 10:22:54 -0400

    Updated swagger and added health check

--- MyLife.Services.API/Program.cs
+++ MyLife.Services.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 using MyLife.Services.API.Infra;
 using MyLife.Services.Shared.Services;
 
@@ -22,7 +23,28 @@ builder.Services.AddCors(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
+builder.Services.AddHealthChecks();
 
 builder.Services.Configure<NotionAppSettings>(builder.Configuration.GetRequiredSection("Notion"));
 
@@ -43,11 +65,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 
     app.UseCors("AllowAll");
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.MapHealthChecks("/health");
 
 app.UseHttpsRedirection();
 
