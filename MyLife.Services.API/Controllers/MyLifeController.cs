1230d5a73cb423b856d5963288fe58ebde36a7cb
Dade Cook <dadecook@gmail.com>
2023-07-06 11:37:14 -0400

    Actual MVP

---
+++ MyLife.Services.API/Controllers/MyLifeController.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+
+namespace MyLife.Services.API.Controllers;
+
+public class MyLifeController : ControllerBase
+{
+    protected readonly IConfiguration configuration;
+
+    public MyLifeController(IConfiguration configuration) => this.configuration = configuration;
+
+    protected async Task<OnBehalfOfCredential> GetOnBehalfOfCredentialAsync()
+    {
+        var tenantId = configuration["AzureAd:TenantId"];
+        var clientId = configuration["AzureAd:ClientId"];
+        var clientSecret = configuration["AzureAd:ClientSecret"];
+
+        var options = new OnBehalfOfCredentialOptions
+        {
+            AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+        };
+
+        var accessToken = await this.HttpContext.GetTokenAsync("access_token");
+
+        return new OnBehalfOfCredential(tenantId, clientId, clientSecret, accessToken, options);
+    }
+}
---
+++ MyLife.Services.API/Controllers/ProfileController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Graph.Models.ODataErrors;
+using Microsoft.Identity.Web.Resource;
+
+namespace MyLife.Services.API.Controllers;
+
+[Authorize]
+[ApiController]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class ProfileController : MyLifeController
+{
+    public ProfileController(IConfiguration configuration)
+        : base(configuration) { }
+
+    [HttpGet("[controller]/Me", Name = "Get My Profile")]
+    public async Task<IActionResult> Me()
+    {
+        var onBehalfOfCredential = await GetOnBehalfOfCredentialAsync();
+        var graphClient = new GraphServiceClient(onBehalfOfCredential);
+        var me = await graphClient.Me.GetAsync();
+
+        return Ok(me);
+
+    }
+
+    [HttpGet("[controller]/Me/Photo", Name = "Get My Profile Photo")]
+    public async Task<IActionResult> MyPhoto()
+    {
+
+        try
+        {
+            var onBehalfOfCredential = await GetOnBehalfOfCredentialAsync();
+            var graphClient = new GraphServiceClient(onBehalfOfCredential);
+            var photo = await graphClient.Me.Photo.Content.GetAsync();
+
+            return Ok(photo);
+        }
+        catch (ODataError oDataError) when (oDataError.Error?.Code == "ImageNotFound")
+        {
+            return NotFound();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+
+    }
+}
